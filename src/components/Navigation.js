74dc4ce6c43b632193dc04dd3c18376d89d9b92e
Satrya Tangguh <satryatp@gmail.com>
2023-02-27 15:50:00 +0700

    create menu bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,35 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
+  let menu
+  let menuMask
+
+  if (showMenu) {
+    menu =
+      <div
+        className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+        The menu
+      </div>
+    
+    menuMask =
+      <div
+        className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+        onClick={() => setShowMenu(false)}>
+
+      </div>
+  }
 
   return (
     <>
       <nav>
         <span className="text-xl">
-          <FontAwesomeIcon icon={faBars}/>
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={() => setShowMenu(!showMenu)}
+          />
         </span>
+        { menuMask }
+        { menu }
       </nav>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'black-t-50': 'rgba(0,0,0,0.5)',
+      }
+    },
   },
   plugins: [],
 }
